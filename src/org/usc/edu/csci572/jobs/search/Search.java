b4b3cd1491d546b2a60d3b610383de77b91aba8f
priyakotwal <p.a.kotwal@gmail.com>
2014-11-09 15:12:49 -0800

    q1

--- src/org/usc/edu/csci572/jobs/search/Search.java
+++ src/org/usc/edu/csci572/jobs/search/Search.java
@@ -1,8 +1,13 @@
 package org.usc.edu.csci572.jobs.search;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashSet;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class Search {
 
@@ -13,23 +18,36 @@ public class Search {
 	public static void main(String[] args) {		
 		Search app = new Search();
 		app.init();
+		try {
+			app.mapLocations();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void init() {
 		regionMap = new HashSet<String>();
 	}
 	
-	public void performQueries() throws Exception {
-		URL u = new URL("");
+	public void performQueries() {
+		
+	}
+	
+	public void mapLocations() throws Exception{
+		URL u = new URL("http://localhost:8983/solr/collection1/select?q=location2%3A*&rows=100&fl=location2&wt=json&indent=true");
         HttpURLConnection c = (HttpURLConnection) u.openConnection();
         c.setRequestMethod("GET"); 
         c.connect();
         int status = c.getResponseCode();
-        
-	}
-	
-	public void mapLocations() {
-		
+        System.out.println(status);
+        if(status == 200 || status == 201) {
+        	InputStream is = c.getInputStream();
+        	JSONParser jsonParser = new JSONParser();
+        	InputStreamReader isr = new InputStreamReader(is);
+        	JSONObject resJson = (JSONObject) jsonParser.parse(isr);
+        	System.out.println(resJson);
+        }
 	}
 	
 	public void query1() {
